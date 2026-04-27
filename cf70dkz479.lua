215b397afbaca8468894c9a463134b39aa22b2ae
nz-cat <truonganhvu1060@gmail.com>
2026-04-27 15:39:21 +0700

    upload cf70dkz479.lua

---
+++ cf70dkz479.lua
@@ -0,0 +1,12 @@
+--// This file was created by XHider v1.2 [https://discord.gg/hATuHQaQRb]
+
+local fenv = getfenv()
+local Ndraawz Protect!! = function() end
+local genv = getgenv()
+genv["Ndraawz Protect!!"] = function() end
+genv["Ndraawz Protect!!"] = function() end
+local Players = game:GetService("Players")
+local _ = fenv.eMIevVs4H43Xe
+local _ = fenv["5Lj016iS7289"]
+local _ = fenv.Z3YtEAMgJy6Px
+local _ = fenv.ZfDP1zfXi5D6GQ

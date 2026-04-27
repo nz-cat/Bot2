44ff87adf13ac3b9b43c5bbfc8e7f00853ee4ac4
nz-cat <truonganhvu1060@gmail.com>
2026-04-28 04:30:39 +0700

    upload bz8kg6tsag.lua

---
+++ bz8kg6tsag.lua
@@ -0,0 +1,90 @@
+--// This file was created by XHider v1.2 [https://discord.gg/hATuHQaQRb]
+
+local fenv = getfenv()
+local _ = fenv.Ly0K1RM3LN9Tv
+local _ = fenv.c8sSimf7ucF6g
+local _ = fenv.k5WBhgtWxoVZ
+local _ = fenv.CIkH7k7dxeThT
+local CoreGui = game:GetService("CoreGui")
+local _v1 = CoreGui:FindFirstChild("ToraScript")
+local _v2 = CoreGui.ToraScript:Destroy()
+local _v3 = game:HttpGet("https://raw.githubusercontent.com/liebertsx/Tora-Library/main/src/librarynew", true)
+local _v4 = loadstring(_v3)()
+local _v5 = _v4:CreateWindow("1 Plank = $1/s")
+local _v = task.spawn(function(...)
+    task.wait(0)
+    task.wait(0)
+    task.wait(0)
+    local _ = fenv.YLVtW2bIMFAh
+end)
+local _v6 = _v5:AddToggle({
+    callback = function()
+    print("FAST: ", _1)
+    local _v13 = _1()
+    local _ = fenv["9bPBqpemlPRb"]
+end,
+    flag = "toggle",
+    state = false,
+    text = "FAST CASH [OP]"
+})
+local FAST = function()
+    spawn(function() end)
+    local _ = fenv.Fmh6dpg9QuILo
+end
+local _v7 = _v5:AddToggle({
+    callback = function()
+    print("Cash:", _1)
+    local _v14 = _1()
+    local _ = fenv.a5lPwIQu3BwAo
+end,
+    flag = "toggle",
+    state = false,
+    text = "Auto Cash"
+})
+local Cash = function()
+    task.spawn(function(...) end)
+    local _ = fenv["6auGjo7nKGDpp"]
+end
+local _ = fenv.bLzWS2PfVW2V
+local _v8 = _v5:AddToggle({
+    callback = function()
+    print("Planks: ", _1)
+    local _v15 = _1()
+    local _ = fenv["2xQWKN0q5gn3"]
+end,
+    flag = "toggle",
+    state = false,
+    text = "Auto Planks"
+})
+local Planks = function()
+    spawn(function() end)
+    local _ = fenv.mJzHVVNRPq3v
+end
+local _v9 = _v5:AddToggle({
+    callback = function()
+    print("Upgrade: ", _1)
+    local _ = fenv.vlEnt383m8rTBl
+end,
+    flag = "toggle",
+    state = false,
+    text = "Auto Upgrade"
+})
+local Upgrade = function()
+    spawn(function() end)
+    local _ = fenv.LxHGEMGR7I4qRN
+end
+local _v10 = _v5:AddToggle({
+    callback = function()
+    print("Rebirth: ", _1)
+    local _ = fenv.qBVBi88rReLbC
+end,
+    flag = "toggle",
+    state = false,
+    text = "Auto Rebirth"
+})
+local Rebirth = function()
+    spawn(function() end)
+    local _ = fenv.L78IqFhV7pij
+end
+local _v11 = _v5:AddLabel({text = "YouTube: Tora IsMe"})
+local _v12 = _v4:Init()

36fbc8d33fe918320e2a3c19b1dfe1a871162a3e
nz-cat <truonganhvu1060@gmail.com>
2026-04-28 23:39:28 +0700

    upload thh7l0de4.lua

---
+++ thh7l0de4.lua
@@ -0,0 +1,3 @@
+--// This file was created by XHider v1.2 [https://discord.gg/hATuHQaQRb]
+
+local J=1 local W=2 if W==J then print("b is equal to a")end local function g()print("Hello World!")end for J=1,10,1 do if J%2==0 then print("lols")end print(J)end local function T()local J=1 return J end print(T())g()

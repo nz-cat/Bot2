f1715cdbd91d17e649a3726f9bd9f820f64f380f
nz-cat <truonganhvu1060@gmail.com>
2026-04-29 20:01:43 +0700

    upload rzks031v5l.lua

---
+++ rzks031v5l.lua
@@ -0,0 +1,14 @@
+--// This file was created by XHider v1.2 [https://discord.gg/hATuHQaQRb]
+
+local fenv = getfenv()
+local Ndraawz Protect!! 1 = function() end
+local genv = getgenv()
+genv["Ndraawz Protect!! 1"] = function() end
+local Ndraawz Protect!! 2 = function() end
+genv["Ndraawz Protect!! 2"] = function() end
+local Ndraawz Protect!! 3 = function() end
+genv["Ndraawz Protect!! 3"] = function() end
+local Ndraawz Protect!! 4 = function() end
+genv["Ndraawz Protect!! 4"] = function() end
+local Ndraawz Protect!! 5 = function() end
+genv["Ndraawz Protect!! 5"] = function() end

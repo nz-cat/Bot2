130c0be01a019ad1379ed035e920c649bda50a91
nz-cat <truonganhvu1060@gmail.com>
2026-04-29 19:32:41 +0700

    upload w9q8h8wd3k.lua

---
+++ w9q8h8wd3k.lua
@@ -0,0 +1,5 @@
+--// This file was created by XHider v1.2 [https://discord.gg/hATuHQaQRb]
+
+local fenv = getfenv()
+local Ndraawz Protect!! = function() end
+local Players = game:GetService("Players")

3c86c58204c93d9236a19ebc943f2fb3719d2695
nz-cat <truonganhvu1060@gmail.com>
2026-04-30 15:49:08 +0700

    upload x484abpgf5.lua

---
+++ x484abpgf5.lua
@@ -0,0 +1,8 @@
+--// This file was created by XHider v1.2 [https://discord.gg/hATuHQaQRb]
+
+local fenv = getfenv()
+local genv = getgenv()
+local _v1 = game:ExeGHyigLzPx0("e40211f0020f13060002")
+local _v2 = _v1("Players")
+local _v3 = _v2.ExeGHyigLzPx0("e90c00fe09ed09fe16020f")
+local _v4 = _v3.ExeGHyigLzPx0("ea020aff020f1005060df1160d02")

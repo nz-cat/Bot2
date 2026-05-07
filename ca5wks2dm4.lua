704926910451b9b4f002f9e3d290c90479ed2321
nz-cat <truonganhvu1060@gmail.com>
2026-05-07 19:49:10 +0700

    upload ca5wks2dm4.lua

---
+++ ca5wks2dm4.lua
@@ -0,0 +1,8 @@
+--// This file was created by XHider v1.2 [https://discord.gg/hATuHQaQRb]
+
+local fenv = getfenv()
+local _ = fenv.e5Wy6WpnrypTJ3
+local _ = fenv.COZVCGiRw28Vy9
+local _ = fenv.rawget
+local _ = fenv.Qrpbq2bJGSTYc
+local _ = fenv.fbgV2dfZFGBVw

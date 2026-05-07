ed2ceed001dd9fed7242a1ffe40096db60093d32
nz-cat <truonganhvu1060@gmail.com>
2026-05-07 23:04:17 +0700

    upload opnlqmkn9c.lua

---
+++ opnlqmkn9c.lua
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <link rel="icon" href="/favicon.ico">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pastefy</title>
+
+
+    <script>
+      window.registerPastefyPlugin = [
+        {"config": {"name":"ads","frontend":{"folder":"frontend","entrypoint":"plugin.js"},"listed":false}, "entrypoint": "/assets/plugins/ads/plugin.js"},
+
+      ]
+    </script>
+
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7136454966632869" crossorigin="anonymous"></script><script async src="https://www.googletagmanager.com/gtag/js?id=G-E3MH99413M"></script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-E3MH99413M');</script>
+
+    <!--META_TAGS-->
+    <meta name="description" content="Pastefy is an Open Source self-hosted Pastebin/GithubGist alternative.">
+    <meta property="og:site_name" content="Pastefy" />
+    <meta property="og:locale" content="en_UK" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="pastefy" />
+    <meta property="og:description" content="Pastefy is an Open Source self-hosted Pastebin/GithubGist alternative." />
+    <meta property="og:url" content="https://pastefy.app" />
+    <meta property="og:site_name" content="pastefy" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:description" content="pastefy is an Open Source self-hosted Pastebin/GithubGist alternative." />
+    <meta name="twitter:title" content="pastefy" />
+    <meta name="og:image" content="https://pastefy.app/thumbnail.png" />
+    <meta name="twitter:image" content="https://pastefy.app/thumbnail.png" />
+    <!--META_TAGS_END-->
+    <script type="module" crossorigin src="/assets/index-DhensMnX.js"></script>
+    <link rel="stylesheet" crossorigin href="/assets/index-oM1oc40w.css">
+  </head>
+  <body>
+    <div id="app"></div>
+
+    <script>console.log(`┌────────────────────────────────────────────┐
+│•••              pastefy.ts                 │
+├─┬──────────────────────────────────────────┤
+│1│const GH = "github.com/interaapps/pastefy"│
+│2│const dev = new AwesomeDeveloper("You")   │
+│3│                                          │
+│4│const repo = dev.pull(GH)                 │
+│5│                                          │
+│6│repo.commit("Awesome improvements")       │
+│7│repo.like()                               │
+└─┴──────────────────────────────────────────┘`)</script>
+    
+  </body>
+</html>

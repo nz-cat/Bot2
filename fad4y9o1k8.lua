3619fa7768af49e6ec330100296681ffcae2f83e
nz-cat <truonganhvu1060@gmail.com>
2026-05-08 20:06:25 +0700

    upload fad4y9o1k8.lua

---
+++ fad4y9o1k8.lua
@@ -0,0 +1,8 @@
+--// This file was created by XHider v1.2 [https://discord.gg/hATuHQaQRb]
+
+local fenv = getfenv()
+local _ = fenv.dK6ThSThx0U4Pp
+local _ = fenv["6j9WtXOeeDv0"]
+local _ = fenv.faKqY5bAqd36jy
+local _ = fenv["0BFHjkgwYmLY6"]
+local _ = fenv.DvXBZ8sOwFxOgE
